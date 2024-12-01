ca45fa8e47cc202e0c5cbab2fbd98a0b2f5f6304
Shakhabutdinov Abdulaziz <shakhabutdinovabdulaziz@gmail.com>
2024-12-01 15:19:23 +0500

    Home page buttons created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EventsComponent } from './pages/events/events.component';
+import { ParticipantsComponent } from './pages/participants/participants.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'events',
+    component: EventsComponent,
+  },
+  {
+    path: 'participants',
+    component: ParticipantsComponent,
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './app/pages/home/home.component';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(HomeComponent, {
+  providers: [importProvidersFrom(RouterModule.forRoot(routes))],
+});
